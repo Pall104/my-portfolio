fd9ca0d022770c8e51bcd78c92e01c8b21b62fb2
Pallavi Nawani <37789944+Pall104@users.noreply.github.com>
2018-04-29 22:49:32 +0530

    updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,30 +6,30 @@ import { HttpClientModule } from '@angular/common/http';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { SuggestionCreateComponent } from './suggestion-create/suggestion-create.component';
+// import { SuggestionCreateComponent } from './suggestion-create/suggestion-create.component';
 
 const appRoutes: Routes = [
 
-    {
-        path: 'suggestion-create',
-        component: SuggestionCreateComponent,
-        data: { title: 'Create Suggestion' }
-    }
+    // {
+    //     path: 'suggestion-create',
+    //     component: SuggestionCreateComponent,
+    //     data: { title: 'Create Suggestion' }
+    // }
    ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    SuggestionCreateComponent
+    // SuggestionCreateComponent
   ],
   imports: [
     BrowserModule,
-      FormsModule,
-      HttpClientModule,
-      RouterModule.forRoot(
-          appRoutes,
-          { enableTracing: true } // <-- debugging purposes only
-      )
+      // FormsModule,
+      // HttpClientModule,
+      // RouterModule.forRoot(
+      //     appRoutes,
+      //     { enableTracing: true } // <-- debugging purposes only
+      // )
   ],
   providers: [],
   bootstrap: [AppComponent]
