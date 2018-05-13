31c5cd358febe2112a1244000f2dce350da25bbb
Pallavi Nawani <37789944+Pall104@users.noreply.github.com>
2018-05-13 22:32:53 +0530

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,29 +23,31 @@ function randomFunction() {
     let w;
     let h;
     let particles;
-    let Particle;;
+    let Particle;
     const resizeReset = function() {
         w = canvasBody.width = window.innerWidth;
         h = canvasBody.height = window.innerHeight;
+        // h = canvasBody.height = window.screen.height;
     };
 
     const opts = {
         particleColor: 'rgb(200,200,200)',
         lineColor: 'rgb(200,200,200)',
-        particleAmount: 40,
+        particleAmount: 20,
         defaultSpeed: 1,
         variantSpeed: 1,
         defaultRadius: 2,
         variantRadius: 2,
         linkRadius: 200
     };
+
     window.addEventListener('resize', function() {
         deBouncer();
     });
 
     const deBouncer = function() {
         clearTimeout(tid);
-        setTimeout(function() {
+        tid = setTimeout(function() {
             resizeReset();
         }, delay);
     };
@@ -53,13 +55,16 @@ function randomFunction() {
     const checkDistance = function(x1, y1, x2, y2) {
         return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
     };
+
     const linkPoints = function(point1, hubs) {
         for (let i = 0; i < hubs.length; i++) {
             const distance = checkDistance(point1.x, point1.y, hubs[i].x, hubs[i].y);
             const opacity = 1 - distance / opts.linkRadius;
             if (opacity > 0) {
                 drawArea.lineWidth = 0.5;
-                drawArea.strokeStyle = `rgba(${rgb[0]}, ${rgb[1]}, ${rgb[2]}, ${opacity})`;
+                drawArea.strokeStyle = `rgba(${rgb[0]}, ${rgb[1]}, ${
+                    rgb[2]
+                    }, ${opacity})`;
                 drawArea.beginPath();
                 drawArea.moveTo(point1.x, point1.y);
                 drawArea.lineTo(hubs[i].x, hubs[i].y);
@@ -75,7 +80,7 @@ function randomFunction() {
         this.speed = opts.defaultSpeed + Math.random() * opts.variantSpeed;
         this.directionAngle = Math.floor(Math.random() * 360);
         this.color = opts.particleColor;
-        this.radius = opts.defaultRadius + Math.random() * opts. variantRadius;
+        this.radius = opts.defaultRadius + Math.random() * opts.variantRadius;
         this.vector = {
             x: Math.cos(this.directionAngle) * this.speed,
             y: Math.sin(this.directionAngle) * this.speed
@@ -85,7 +90,7 @@ function randomFunction() {
             this.x += this.vector.x;
             this.y += this.vector.y;
         };
-        this.border = function(){
+        this.border = function() {
             if (this.x >= w || this.x <= 0) {
                 this.vector.x *= -1;
             }
@@ -93,9 +98,9 @@ function randomFunction() {
                 this.vector.y *= -1;
             }
             if (this.x > w) { this.x = w; }
-            if (this.y > h) {this.y = h; }
-            if (this.x < 0) {this.x = 0; }
-            if (this.y < 0) {this.y = 0; }
+            if (this.y > h) { this.y = h; }
+            if (this.x < 0) { this.x = 0; }
+            if (this.y < 0) { this.y = 0; }
         };
         this.draw = function() {
             drawArea.beginPath();
@@ -110,14 +115,14 @@ function randomFunction() {
         particles = [];
         resizeReset();
         for (let i = 0; i < opts.particleAmount; i++) {
-            particles.push( new Particle() );
+            particles.push(new Particle());
         }
         window.requestAnimationFrame(loop);
     }
 
     function loop() {
         window.requestAnimationFrame(loop);
-        drawArea.clearRect(0, 0 , w , h);
+        drawArea.clearRect(0, 0, w, h);
         for (let i = 0; i < particles.length; i++) {
             particles[i].update();
             particles[i].draw();
@@ -129,18 +134,9 @@ function randomFunction() {
 
     const canvasBody: any = document.getElementById('canvas');
     const drawArea = canvasBody.getContext('2d');
-    const delay = 200, tid = 0,
-        rgb = opts.lineColor.match(/\d+/g);
+    const delay = 200;
+    let tid;
+    const rgb = opts.lineColor.match(/\d+/g);
     resizeReset();
     setup();
 }
-
-function openNav() {
-    document.getElementById('mySidenav').style.width = '250px';
-}
-
-function closeNav() {
-    document.getElementById('mySidenav').style.width = '0';
-}
-
-
